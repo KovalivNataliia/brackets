6025ea3d73618ff72a0401ae9c7de6fa3fbc38af
NataliiaKovaliv <nataliiakovaliv95@gmail.com>
2021-07-20 15:45:08 +0300

    feat: implement function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  bracketsConfig = bracketsConfig.map(item => item.join(''));
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    if (str.includes(bracketsConfig[i])) {
+      str = str.replace(bracketsConfig[i], '')
+      i = -1;
+    }
+  }
+  
+  return str.length === 0 ? true : false;
 }
